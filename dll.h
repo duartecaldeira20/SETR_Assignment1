cb973b5299effae64956bddfa34750102107be38
duartecaldeira20 <duartecaldeira@ua.pt>
2025-03-11 10:54:03 +0000

    dll.h not defined yet

---
+++ dll.h
@@ -0,0 +1,48 @@
+#ifndef DLL_H
+#define DLL_H
+
+#include<stdint.h>
+
+typedef struct
+{
+	
+	
+	
+} DLL;
+
+/**
+*/
+
+int MyDLLInit()
+{}
+
+/**
+*/
+
+int MyDLLInsert()
+{}
+
+/**
+*/
+
+int MyDLLRemove()
+{}
+
+/**
+*/
+
+int MyDLLFind()
+{}
+
+/**
+*/
+
+int MyDLLFindNext()
+{}
+
+/**
+*/
+
+int MyDLLFindPrevious()
+{}
+
